c2254964b4ad7d523a71749ccd9e88ddf0a1260b
William <williambassedone@outlook.com>
2025-06-26 11:24:29 -0300

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,33 @@
-export async function getServerSideProps() {
-  return {
-    props: {
-      hora: new Date().toLocaleString()
-    }
-  }
-}
+// export async function getServerSideProps() {
+//   return {
+//     props: {
+//       hora: new Date().toLocaleString()
+//     }
+//   }
+// }
 
-export default function Home({ hora }) {
+// export default function Home({ hora }) {
+//   return (
+//     <div style={{ fontFamily: 'sans-serif', padding: 20 }}>
+//       <h1>SSR funcionando! 🎉</h1>
+//       <p>Hora gerada no servidor: {hora}</p>
+//     </div>
+//   )
+// }
+
+
+
+import Link from 'next/link'
+
+export default function Home() {
   return (
-    <div style={{ fontFamily: 'sans-serif', padding: 20 }}>
-      <h1>SSR funcionando! 🎉</h1>
-      <p>Hora gerada no servidor: {hora}</p>
+    <div>
+      <h1>Página Inicial</h1>
+      <p>
+        <Link href="/sobre">
+          Ir para a página Sobre
+        </Link>
+      </p>
     </div>
   )
 }
---
+++ src/pages/sobre.js
@@ -0,0 +1,8 @@
+export default function Sobre() {
+  return (
+    <div>
+      <h1>Página Sobre</h1>
+      <p>Esta página é renderizada sem reload!</p>
+    </div>
+  )
+}
