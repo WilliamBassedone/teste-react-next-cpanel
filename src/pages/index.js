81f34048752eb48dadb0a2b71ce10a7196fcbdce
William <williambassedone@outlook.com>
2025-06-26 11:41:17 -0300

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,15 +16,14 @@
 // }
 
 
-
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
-      <h1>Página Inicial</h1>
+      <h1 className="text-white">Página Inicial</h1>
       <p>
-        <Link href="/sobre">
+        <Link href="/sobre" className="text-blue-500 hover:text-blue-700">
           Ir para a página Sobre
         </Link>
       </p>
